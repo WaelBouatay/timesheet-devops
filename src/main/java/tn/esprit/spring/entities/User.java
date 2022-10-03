8b607328e740f4e8cd594b5d1157accb2e98c780
Wael <wael.bouatay@esprit.tn>
2022-10-03 12:00:25 +0100

    modif 2

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -20,12 +20,9 @@ public class User implements Serializable {
 	
 	// commentaitre 3ALINFO5 
 	
-	// commentaire 1 
-	// Commentaire 2 
+
 	
-	// Commaentaire 4
-	// hhhh
-	// modif 5
+
 	private static final long serialVersionUID = 1L;
 
 	@Id
